fdee8aeecb27b1737fd2ec81633d21f262c3eb03
johmara <johan.t.martinson@gmail.com>
2025-11-27 15:37:33 +0100

    Remove Firebase credentials from environment files
    Revert to placeholder values. Credentials should only be injected at
    build time via set-env.js script using GitHub secrets or local .env file.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
-    apiKey: 'AIzaSyBFZqkCqWXtKU4PEzIzjYZLf5ZZSBrfczc',
-    authDomain: 'contraction-timer-40d06.firebaseapp.com',
-    projectId: 'contraction-timer-40d06',
-    storageBucket: 'contraction-timer-40d06.firebasestorage.app',
-    messagingSenderId: '709733949527',
-    appId: '1:709733949527:web:ee2d1655ec2e3e37e4e882'
+    apiKey: 'YOUR_API_KEY',
+    authDomain: 'YOUR_PROJECT_ID.firebaseapp.com',
+    projectId: 'YOUR_PROJECT_ID',
+    storageBucket: 'YOUR_PROJECT_ID.appspot.com',
+    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+    appId: 'YOUR_APP_ID'
   }
 };
